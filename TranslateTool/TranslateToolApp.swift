89fa5fa17d11cb6c421f937e641b5d2bb22da14d
Austin <austin20010423@gmail.com>
2026-02-24 18:02:42 -0700

    Initial Commit

---
+++ TranslateTool/TranslateToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateToolApp.swift
+//  TranslateTool
+//
+//  Created by 陳肇翔 on 2026/2/24.
+//
+
+import SwiftUI
+
+@main
+struct TranslateToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
